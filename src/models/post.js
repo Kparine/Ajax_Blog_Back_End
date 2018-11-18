594df20c70a36fd90a7f9478b21782a4edffe9d2
Kevin Parine <kparine314@gmail.com>
2018-11-18 15:36:45 -0800

    update tests

--- src/models/post.js
+++ src/models/post.js
@@ -39,26 +39,26 @@ function getOne(id) {
 
 //CREATE FUNCTION
 function create(newPost) {
-  const error = []
+  const errors = []
   const {
     title,
     content
   } = newPost
 
+  //TEST CASES FOR CREATE FUNCTION
   if (!title || !content) {
-    error.push('Please Provide Title And Content')
+    errors.push('Please Provide Title And Content')
   }
-
-  // if(title.length > 30){
-  //   error.push('Title Should Be Less Than 30 Characters')
-  // }
-  // if(content.length > 300){
-  //   error.push('Post Should Less Than 300 Characters')
-  // }
-  // if (error.length) return {
-  //   error
-  // }
-
+  if(title.length > 30){
+    errors.push('Title Should Be Less Than 30 Characters')
+  }
+  if(content.length > 300){
+    errors.push('Post Should Less Than 300 Characters')
+  }
+  if (errors.length) return {
+    errors
+  }
+  
   let post = {
     id: uuid(),
     title,
@@ -72,29 +72,33 @@ function create(newPost) {
 
 //UPDATE FUNCTION
 function update(id, body) {
-  const error = []
+  const errors = []
   const { title, content } = body
   const posts = read()
   const post = posts.find(ele => ele.id === id)
 
   if (post < 0) {
-    error.push('Could Not Find Post To Edit')
+    errors.push('Could Not Find Post To Edit')
+  }
+  if (!title || !content) {
+    errors.push('Please Provide Title And Content')
+  }
+  if(title.length > 30){
+    errors.push('Title Should Be Less Than 30 Characters')
+  }
+  if(content.length > 300){
+    errors.push('Post Should Less Than 300 Characters')
+  }
+  if (errors.length) return {
+    errors
   }
 
-  if (!title) {
-    error.push('Posts Require A Title')
-  }
-
-  if (!content) {
-    error.push('Posts Require Content')
-  }
-
-  if (error.length < 1) {
+  if (errors.length < 1) {
     post.title = title
     post.content = content
     write(posts)
   } else {
-    post.error = error
+    post.errors = errors
   }
   return post
 }
